554d5525d664875ade0e7397624fecf79daa702d
xmlwizard <xml-wizard@outlook.com>
2024-12-04 07:04:34 +0200

    added appbar component

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {} from "flowbite-react";
+function Navbar() {
+    return (
+        <>
+            <h1>Dean</h1>
+        </>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Navbar;
